3557bea70d5e9099a6e8f9623afe45140da7820f
2minRain <1207826398@qq.com>
2025-01-04 11:17:36 +0800

    迁移图片&ts初步支持

---
+++ src/ts/exports.js
@@ -0,0 +1,6 @@
+import router from '@system.router';
+import storage from '@system.storage';
+import prompt from '@system.prompt';
+import file from '@system.file';
+
+export { storage, router, prompt, file };
---
+++ src/ts/oobe/init.ts
@@ -0,0 +1,38 @@
+import { file, prompt, storage, router } from '../exports'
+export function init() {
+    file.writeText({
+        uri: 'internal://files/cora-todolist.json.txt',
+        text: '{}',
+        success: () => {
+          console.log('保存成功');
+        },
+        fail: (data, code) => {
+          console.log(`handling fail, code = ${code}`);
+          prompt.showToast({
+            message: `初始化错误, 错误码${code}`,
+            duration: 1500
+          })
+        }
+      });
+      storage.set({
+        key: 'app_version',
+        value: '2.0.1'
+      });
+      storage.set({
+        key: 'todojs_version',
+        value: '0.1.0-quickjs-vela-quickapp',
+        success: () => {
+          nextpage()
+        }
+      });
+      storage.set({
+        key: 'inited',
+        value: 'inited'
+      })
+}
+
+function nextpage() {
+    router.replace({
+      uri: "/pages/index"
+    })
+  }
